9f8d03ec63ff1912180391a4073c4094130c322c
Elaina Sorenson <78282104+Dezigncat89@users.noreply.github.com>
2021-05-14 08:57:57 -0700

    Update Bomb.js

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,28 +1,60 @@
-import React, { Component } from 'react'
-class Bomb extends Component {
-
+class Bomb extends React.Component {
   constructor() {
-    super()
-    this.state = {
-      count: 0
+    super();
+    this.state = { time: {}, seconds: 60 };
+    this.timer = 0;
+    this.startTimer = this.startTimer.bind(this);
+    this.countDown = this.countDown.bind(this);
+  }
+
+  secondsToTime(secs){
+    let hours = Math.floor(secs / (60 * 60));
+
+    let divisor_for_minutes = secs % (60 * 60);
+    let minutes = Math.floor(divisor_for_minutes / 60);
+
+    let divisor_for_seconds = divisor_for_minutes % 60;
+    let seconds = Math.ceil(divisor_for_seconds);
+
+    let obj = {
+      "h": hours,
+      "m": minutes,
+      "s": seconds
+    };
+    return obj;
+  }
+
+  componentDidMount() {
+    let secondsLeft = this.secondsToTime(this.state.seconds);
+    this.setState({ time: secondsLeft });
+  }
+
+  startTimer() {
+    if (this.timer == 0 && this.state.seconds > 0) {
+      this.timer = setInterval(this.countDown, 1000);
     }
   }
 
-  increment = () => {
-    console.log(`before setState: ${this.state.count}`)
-
+  countDown() {
+    let seconds = this.state.seconds - 1;
     this.setState({
-      count: this.state.count + 1
-    })
-
-    console.log(`after setState: ${this.state.count}`)
+      time: this.secondsToTime(seconds),
+      seconds: seconds,
+    });
+    
+    if (seconds == 0) { 
+      clearInterval(this.timer);
+    }
   }
 
   render() {
-    return (
-      <div onClick={this.increment}>
-        {this.state.count}
+    return(
+      <div>
+        <button onClick={this.startTimer}>Start Bomb</button>
+        m: {this.state.time.m} s: {this.state.time.s}
       </div>
-    )
+    );
   }
 }
+
+export default Bomb
