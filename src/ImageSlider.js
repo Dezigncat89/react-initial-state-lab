4a602618c3e875bb9ec1338cb87e14594d3a9d44
Elaina Sorenson <78282104+Dezigncat89@users.noreply.github.com>
2021-05-14 08:03:34 -0700

    Update ImageSlider.js

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,9 @@
-// your ImageSlider code here!
+import React, { Component } from 'react'
+  class ImageSlider extends Component {
+    constructor(props) {
+    super(props)
+    this.state = {
+      currentSlideIndex: 0
+    }
+  }
+}
