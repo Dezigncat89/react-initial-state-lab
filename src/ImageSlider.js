1e7702da776a09c645507586af4cefe0b2a775de
Elaina Sorenson <78282104+Dezigncat89@users.noreply.github.com>
2021-05-14 08:45:09 -0700

    Update ImageSlider.js

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react'
-  class ImageSlider extends Component {
-    constructor(props) {
+ class ImageSlider extends Component {
+
+  constructor(props) {
     super(props)
-    this.state = {
+    state = {
       currentSlideIndex: 0
     }
   }
 }
+
+ slideIncrement = () => {
+  this.setState({ currentSlideIndex: this.state.currentSlideIndex + 1 })
+}
+
+slideDecrement = () => {
+  this.setState({ currentSlideIndex: this.state.currentSlideIndex - 1})
+
+  render() {
+  return(
+    <div>
+    <div>
+      {this.state.currentSlideIndex}
+    </div>
+    <button onClick ={this.slideIncrement}>Increment by 1</button>
+    <button onClick ={this.slideDecrement}>Decrement by 1</button>
+      )
+    }
+ }
+
+
