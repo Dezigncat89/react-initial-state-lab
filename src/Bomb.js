16b93c728e17466feb37c7c1b29275c4c845f9ff
Elaina Sorenson <78282104+Dezigncat89@users.noreply.github.com>
2021-05-14 08:04:56 -0700

    Update Bomb.js

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,28 @@
-// your Bomb code here!
+import React, { Component } from 'react'
+class Bomb extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      count: 0
+    }
+  }
+
+  increment = () => {
+    console.log(`before setState: ${this.state.count}`)
+
+    this.setState({
+      count: this.state.count + 1
+    })
+
+    console.log(`after setState: ${this.state.count}`)
+  }
+
+  render() {
+    return (
+      <div onClick={this.increment}>
+        {this.state.count}
+      </div>
+    )
+  }
+}
